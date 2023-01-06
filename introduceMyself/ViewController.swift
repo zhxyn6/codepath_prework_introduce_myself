2ba5befe009a36f342f4fe5afbbe7a9a9fd8ab46
Xinran <zhxyn6@gmail.com>
2023-01-06 11:18:36 -0500

    initial UI set up

---
+++ introduceMyself/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  introduceMyself
+//
+//  Created by Yujun Zhao on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
