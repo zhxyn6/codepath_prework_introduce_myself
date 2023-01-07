8a1f0152554b74434b3201ebcc7787b552d34ee6
Xinran <zhxyn6@gmail.com>
2023-01-07 13:24:51 -0500

    add introduction functions
    - Improve the UI by using different fonts/colors/background
    - Research persistence to learn how to store the information so when app reboots the information remains
    - Add more fields to crease a more thorough introduction

--- introduceMyself/ViewController.swift
+++ introduceMyself/ViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var morePetSwitch: UISwitch!
+    @IBOutlet weak var morePetSteppers: UIStepper!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
-    
     @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var numberOfPetLabel: UILabel!
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func introduceDidTapped(_ sender: UIButton){
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I am currently a \(year!) year student in \(schoolNameTextField.text!). I own \(numberOfPetLabel.text!) dogs. It is \(morePetSwitch.isOn) that I want more pets."
+        
+//        print(introduction)
+        
+        // Creates the alert where we pass in our message, which our introduction.
+        let alertController = UIAlertController(title: "My Introduction", message:introduction, preferredStyle: .alert)
+                
+        // A way to dismiss the box once it pops up
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler:nil)
+                
+        // Passing this action to the alert controller so it can be dismissed
+        alertController.addAction(action)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
